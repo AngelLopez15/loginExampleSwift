34f0d770b1ba92ae0af2dfe01767b0e59ece8b57
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-03-29 15:51:11 -0600

    Initial Commit

---
+++ ExampleLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleLogin
+//
+//  Created by Angel Octavio Lopez Cruz on 29/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
